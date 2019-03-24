a3e34b26bad4e1496b4b3cc566ed4da256319459
Jun <geffrey90@hotmail.com>
2019-03-25 01:33:28 +0800

    added comments

--- main.go
+++ main.go
@@ -5,18 +5,18 @@ import "fmt"
 func main() {
 	var s int
 	var f string
-	for c := true; c; c = (s != 5) {
-		getSelection(&s)
-		switch s {
-		case 1:
+	for c := true; c; c = (s != 5) { //do while loop for golang. initial condition always true
+		getSelection(&s) //call getSelection function to scan standard input passing pointer s to function as argument
+		switch s {       //switch statement for s
+		case 1: //if s is 1
 			f = "Apple"
-		case 2:
+		case 2: //if s is 2
 			f = "Kiwi"
-		case 3:
+		case 3: //if s is 3
 			f = "Melon"
-		case 4:
+		case 4: //if s is 4
 			f = "Banana"
-		case 5:
+		case 5: //if s is 5
 			fmt.Println("Terminating program")
 		default:
 			fmt.Println("Selected option is not available")
